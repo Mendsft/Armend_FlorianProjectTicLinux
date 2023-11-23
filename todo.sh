128c7efe34b4b4ddd7f8e21d3fed4b7517ab7dbe
afetahi <fetahiarmend@gmail.com>
2023-11-23 16:58:42 +0100

    ebauche de projet

--- todo.sh
+++ todo.sh
@@ -1,5 +1,27 @@
 #To Do list a faire main 
 
 #!/bin/bash
+echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter , supprimer  ou afficher certaines tache , Que voulez vous faire ? (a/s/d) "
+read choix
 
+while [[ "$choix" =~ [asd] ]] #boucle qui nous dmd la reponse tant qu'on 
+do
+    if [[ "$choix " =~ [a] ]]
+    then
+        echo "Test =A"
+        break
 
+    elif [[ "$choix " =~ [s] ]]
+    then
+        echo "Test =b"
+        break
+
+    elif [[ "$choix " =~ [d] ]]
+    then
+        echo "Test =C"
+        break
+    
+    fi
+done
+
+echo "oke merci d'etre passer"
