365a89cc7d655e38cc895cf1ac5c34ec216308b9
afetahi <fetahiarmend@gmail.com>
2023-11-23 23:40:39 +0100

    Ajouter v1.3

--- todo.sh
+++ todo.sh
@@ -1,45 +1,39 @@
 #To Do list a faire main 
 
 #!/bin/bash
-
-while [[ "$choix" != [123]  ]] #boucle qui nous dmd la reponse tant qu'on 
-do
+i=1
+while true ; do #boucle qui nous dmd la reponse tant qu'on 
     echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter(1) , supprimer(2)  ou afficher certaines tache(2) , Que voulez vous faire ? (1/2/3) "
     read choix
+    touch task
 
-        if [[ "$choix " =~ [1] ]] #Si le choix de l'utilisateur est "1" alors on execute ce code
-        then
-            
-            printf "\n"
-            printf "Entrez le nom de la tâche"
-            printf "\n : "
-            read -r topic
-            printf "\n"
-            printf "Entrez\n "
-            printf "\n : "
-            read -r entrez
-        if [[ -f "./$topic" ]]; then
-            printf '[ ] %s\n' "$entrez" >> "./$topic"
-            nano "./$topic"
-        else
-            touch "./$topic"
+if [[ "$choix " =~ [1] ]]; then #Si le choix de l'utilisateur est "1" alors on execute ce code
+    printf "\n"
+    printf "tache\n "
+    read -r tache
 
-            printf '\n %s\n' "$entrez" >> "./$topic"
-            nano "./$topic"
-        fi
-            
+    if [[ -f "./task" ]]; then
+        printf " [ $i ] %s\n" "$tache" >> "./task"
+        nano "./task"
+        ((i++))
 
-        elif [[ "$choix " =~ [2] ]]
-        then
-            echo "Test =b"
+    else
+        printf "[ $i ] %s\n" "$tache" >> "./task"
+        nano "./task"
+        ((i++))
+    fi
 
 
-        elif [[ "$choix " =~ [3] ]]
-        then
-            echo "Test =C"
+elif [[ "$choix " =~ [2] ]]
+then
+    echo "Test =b"
+elif [[ "$choix " =~ [3] ]]
+then
+    echo "Test =C"
+fi
 
-        
-        fi
+    
 done
+
 
 echo "oke merci d'etre passé"
