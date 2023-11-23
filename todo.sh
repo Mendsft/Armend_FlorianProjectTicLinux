74d41982528c19fb142b41a822d4cd4c51327198
afetahi <fetahiarmend@gmail.com>
2023-11-23 21:40:25 +0100

    Ajouter une tache v.1

--- todo.sh
+++ todo.sh
@@ -7,9 +7,26 @@ do
     echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter(1) , supprimer(2)  ou afficher certaines tache(2) , Que voulez vous faire ? (1/2/3) "
     read choix
 
-        if [[ "$choix " =~ [1] ]]
+        if [[ "$choix " =~ [1] ]] #Si le choix de l'utilisateur est "1" alors on execute ce code
         then
-            echo "Test =A"
+            
+            printf "\n"
+            printf "Entrez le nom de la tâche"
+            printf "\n : "
+            read -r topic
+            printf "\n"
+            printf "Entrez\n "
+            printf "\n : "
+            read -r entrez
+        if [[ -f "./$topic" ]]; then
+            printf '[ ] %s\n' "$entrez" >> "./$topic"
+            nano "./$topic"
+        else
+            touch "./$topic"
+
+            printf '\n %s\n' "$entrez" >> "./$topic"
+            nano "./$topic"
+        fi
             
 
         elif [[ "$choix " =~ [2] ]]
