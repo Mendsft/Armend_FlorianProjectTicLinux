97880073095d15106e9c2905e10980f04d0b5eae
afetahi <fetahiarmend@gmail.com>
2023-11-23 18:18:25 +0100

    amelioration bouchle v1.1

--- todo.sh
+++ todo.sh
@@ -2,22 +2,22 @@
 
 #!/bin/bash
 
-while [[ "$choix" != [asd] ]] #boucle qui nous dmd la reponse tant qu'on 
+while [[ "$choix" != [123]  ]] #boucle qui nous dmd la reponse tant qu'on 
 do
-    echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter , supprimer  ou afficher certaines tache , Que voulez vous faire ? (a/s/d) "
+    echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter(1) , supprimer(2)  ou afficher certaines tache(2) , Que voulez vous faire ? (1/2/3) "
     read choix
 
-        if [[ "$choix " =~ [a] ]]
+        if [[ "$choix " =~ [1] ]]
         then
             echo "Test =A"
             
 
-        elif [[ "$choix " =~ [s] ]]
+        elif [[ "$choix " =~ [2] ]]
         then
             echo "Test =b"
 
 
-        elif [[ "$choix " =~ [d] ]]
+        elif [[ "$choix " =~ [3] ]]
         then
             echo "Test =C"
 
