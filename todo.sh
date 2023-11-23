691a794a2f1148d08f2f9285d3d6311904c709fc
afetahi <fetahiarmend@gmail.com>
2023-11-23 18:08:54 +0100

    amelioration de la boucle v1

--- todo.sh
+++ todo.sh
@@ -1,27 +1,28 @@
 #To Do list a faire main 
 
 #!/bin/bash
-echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter , supprimer  ou afficher certaines tache , Que voulez vous faire ? (a/s/d) "
-read choix
 
-while [[ "$choix" =~ [asd] ]] #boucle qui nous dmd la reponse tant qu'on 
+while [[ "$choix" != [asd] ]] #boucle qui nous dmd la reponse tant qu'on 
 do
-    if [[ "$choix " =~ [a] ]]
-    then
-        echo "Test =A"
-        break
+    echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter , supprimer  ou afficher certaines tache , Que voulez vous faire ? (a/s/d) "
+    read choix
 
-    elif [[ "$choix " =~ [s] ]]
-    then
-        echo "Test =b"
-        break
+        if [[ "$choix " =~ [a] ]]
+        then
+            echo "Test =A"
+            
 
-    elif [[ "$choix " =~ [d] ]]
-    then
-        echo "Test =C"
-        break
-    
-    fi
+        elif [[ "$choix " =~ [s] ]]
+        then
+            echo "Test =b"
+
+
+        elif [[ "$choix " =~ [d] ]]
+        then
+            echo "Test =C"
+
+        
+        fi
 done
 
-echo "oke merci d'etre passer"
+echo "oke merci d'etre passé"
