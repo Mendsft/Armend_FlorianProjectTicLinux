05c93f8649f22d593155bd782e837b0049710fa2
afetahi <fetahiarmend@gmail.com>
2023-11-24 14:56:31 +0100

    Version presque finale

--- todo.sh
+++ todo.sh
@@ -6,7 +6,8 @@ i=1
 while true ; do #boucle qui nous dmd la reponse tant qu'on
     
     echo " "
-    echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter(1) , supprimer(2)  ou afficher certaines tache(3) ou bien tout supprimer (5) , Que voulez vous faire ? (1/2/3/5) "
+    echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter(1) , supprimer(2)  ou afficher certaines tache(3), Modifier les tâches(4) ou bien tout supprimer (5) , Que voulez vous faire ? (1/2/3/4/5) "
+    echo " "
     read choix
     touch task
 if [[ "$choix " =~ [1] ]]; then #Si le choix de l'utilisateur est "1" alors on execute ce code et on ajoute des taches aux fichier task
@@ -35,7 +36,7 @@ then
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     read -p "Entrez le numéro de la tâche à supprimer : " numero
     if [ "$(gsed -n "/${numero}/p" ./task )" ]; then
-        gsed -i "/${numero}/d" ./task #q Sed fait la recherche du nombre encodé et la supprime.
+        gsed -i "/${numero}/d" ./task #gsed fait la recherche du nombre encodé et la supprime.
         echo "~~~~~~~~~~~~~~~~~~~~~~"
         echo "Tâche supprimée avec succès !"
         echo "~~~~~~~~~~~~~~~~~~~~~~"
@@ -66,14 +67,21 @@ then
     echo "  "
     echo " --------------------------------------"
 
-    
-elif [[ "$choix" =~ [5] ]] # Supprimes toute le contenu du fichier 
+elif [[ "$choix" =~ [4] ]]
 then
-    truncate -s 0 task  
+    nano task
+
+elif [[ "$choix" =~ [5] ]] # Supprime tout le contenu du fichier 
+then
+    echo " "
+    echo "Vous venez de supprimer toutes les taches :/ "
+    truncate -s 0 task
+    echo " "  
 
 
 elif [[ "$choix" =~ [q/Q] ]] # quitte la boucle de Todolsit
 then
+    echo " "
     echo "aurevoir merci"
     break
 fi   
