45dc191d1d23b18a26c89a20a0644daaa02a969c
afetahi <fetahiarmend@gmail.com>
2023-11-24 10:55:32 +0100

    Version beta

--- todo.sh
+++ todo.sh
@@ -5,30 +5,39 @@ i=1
 while true ; do #boucle qui nous dmd la reponse tant qu'on 
     echo "Bonjour,bienvenu dans votre to do list, vous pouvers ajouter(1) , supprimer(2)  ou afficher certaines tache(3) ou bien tout supprimer (5) , Que voulez vous faire ? (1/2/3/5) "
     read choix
-    touch task
+    # touch task
 
-if [[ "$choix " =~ [1] ]]; then #Si le choix de l'utilisateur est "1" alors on execute ce code
+if [[ "$choix " =~ [1] ]]; then #Si le choix de l'utilisateur est "1" alors on execute ce code et on ajoute des taches aux fichier task
     printf "\n"
     printf "tache\n "
     read -r tache
 
-    if [[ -f "./task" ]]; then
-        printf " [ $i ] %s\n" "$tache" >> "./task"
+    if [[ -f "./task" ]]; then  #
+        printf " $i %s\n" "$tache" >> "./task" 
         nano "./task"
         ((i++))
 
     else
-        printf "[ $i ] %s\n" "$tache" >> "./task"
+        printf " $i  %s\n" "$tache" >> "./task"
         nano "./task"
         ((i++))
     fi
 
 
-elif [[ "$choix " =~ [2] ]]
+elif [[ "$choix " =~ [2] ]] #si l'utilisateur rentre 2 , alors on va lancer le code suivant pour supprimer via le numero de la tache
 then
-    printf "Quel tache voulez vous supprimer ?"
-    read -r nbr
-    $ grep -v "$i" mtask > tmpfile && mv tmpfile task
+
+ read -p "Entrez le numéro de la tâche à supprimer : " numero
+    if [ "$(gsed -n "${numero}p" ./task )" ]; then
+        gsed -i "${numero}d" ./task #q Sed fait la recherche du nombre encodé et la supprime.
+        echo "~~~"
+        echo "Tâche supprimée avec succès !"
+        echo "~~~"
+    else
+        echo "~~~"
+        echo "La tâche n'existe pas."
+        echo "~~~"
+    fi
 
 
 
@@ -38,12 +47,12 @@ then
     echo "Voici ce que contient votre To-do-list"
     cat task
     
-elif [[ "$choix" =~ [5] ]]
+elif [[ "$choix" =~ [5] ]] # Supprimes toute le contenu du fichier 
 then
-    truncate -s 0 task
+    truncate -s 0 task  
 
 
-elif [[ "$choix" =~ [q/Q] ]]
+elif [[ "$choix" =~ [q/Q] ]] # quitte la boucle de Todolsit
 then
     echo "aurevoir merci"
     break
